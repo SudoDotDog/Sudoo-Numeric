fc4fa832e5fd109812828c8742eeb3fc722ef438
WMXPY <wm@mengw.io>
2019-09-11 16:14:53 -0500

    implement csv parsing

---
+++ src/util.ts
@@ -0,0 +1,39 @@
+/**
+ * @author WMXPY
+ * @namespace Numeric
+ * @description Util
+ */
+
+export const parseCSVLine = (line: string): string[] => {
+
+    const result: string[] = [];
+    let quoting: boolean = false;
+    let buffer: string = '';
+    for (const char of line) {
+
+        if (char === ',') {
+
+            if (quoting) {
+                buffer += char;
+            } else {
+                result.push(buffer);
+                buffer = '';
+            }
+            continue;
+        }
+
+        if (char === '"') {
+
+            if (quoting) {
+                quoting = false;
+            } else {
+                quoting = true;
+            }
+            continue;
+        }
+
+        buffer += char;
+    }
+
+    return result;
+};
