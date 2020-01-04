dfafec80e9f79e0ac2775f8372aea557b26e279e
WMXPY <wm@mengw.io>
2020-01-05 00:46:39 +0800

    add transform function

---
+++ src/math/transform.ts
@@ -0,0 +1,12 @@
+/**
+ * @author WMXPY
+ * @namespace Numeric_Math
+ * @description Transform
+ */
+
+// tslint:disable-next-line: variable-name
+export const UNSAFE_transformToInteger = (target: number): number => {
+
+    // tslint:disable-next-line: no-bitwise
+    return ~~target;
+};
