f51e0c9802e33e0db41f6c0e35b261ceda9aee06
WMXPY <wm@mengw.io>
2020-02-23 18:28:04 -0600

    finish structure deploy, fix import

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@
 
 export * from "./csv/builder";
 export * from "./csv/csv";
+export * from "./csv/util";
 export * from "./declare";
 export * from "./math/calculate";
 export * from "./math/transform";
 export * from "./number/deconstruction";
 export * from "./shuffle/array";
+export * from "./shuffle/structure";
 export * from "./unit/binary";
 
--- src/shuffle/structure.ts
+++ src/shuffle/structure.ts
@@ -15,12 +15,30 @@ export type FieldBasedDeployStructuralArrayElement<T extends Record<string, any>
 
 export const fieldBasedDeployStructuralArray = <T extends Record<string, any>, K extends keyof T>(original: T[], fieldName: K, cloneFunction?: CloneFunction<T>): Array<FieldBasedDeployStructuralArrayElement<T, K>> => {
 
-    const result: Array<FieldBasedDeployStructuralArrayElement<T, K>> = [];
+    const medium: Record<T[K], T[]> = {} as Record<T[K], T[]>;
     if (original.length <= 0) {
-        return result;
+        return [];
     }
 
     const parsed: T[] = cloneFunction ? original.map(cloneFunction) : original;
+    for (const each of parsed) {
+        const value: T[K] = each[fieldName];
+        if (medium[value]) {
+            medium[value].push(each);
+        } else {
+            medium[value] = [each];
+        }
+    }
+
+    const result: Array<FieldBasedDeployStructuralArrayElement<T, K>> = [];
+    const keys: Array<T[K]> = Object.keys(medium) as Array<T[K]>;
+    for (const key of keys) {
+        result.push({
+            fieldName,
+            fieldValue: key,
+            children: medium[key],
+        });
+    }
 
     return result;
 };
