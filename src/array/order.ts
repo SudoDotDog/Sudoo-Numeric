d7d61b0a9d5d65d784af9973663b3765cdc1772d
WMXPY <wm@mengw.io>
2020-04-08 15:28:31 -0500

    implement array order function

---
+++ src/array/order.ts
@@ -0,0 +1,21 @@
+/**
+ * @author WMXPY
+ * @namespace Numeric_Array
+ * @description Order
+ */
+
+export const car = <T extends any>(target: T[]): T => {
+
+    if (target.length <= 0) {
+        return null as any;
+    }
+    return target[0] as T;
+};
+
+export const cdr = <T extends any>(target: T[]): T[] => {
+
+    if (target.length <= 1) {
+        return [];
+    }
+    return target.slice(1);
+};
