5731ad5707c6d59e5fd8c074ecf5ac1e8dab5c0a
WMXPY <wm@mengw.io>
2020-09-03 14:31:34 -0500

    finish calculate average function

--- src/array/calculate.ts
+++ src/array/calculate.ts
@@ -4,6 +4,36 @@
  * @description Calculate
  */
 
-export const calculateArrayAverage = <T extends any = any>(list: T[],): number => {
+import { ExtractFeatureFunction } from "../declare";
 
+export const calculateArrayAverage = <T extends any = any>(list: T[], extractFeatureFunction?: ExtractFeatureFunction<T, number>): number => {
+
+    if (!Array.isArray(list)) {
+        return NaN;
+    }
+
+    const total: number = list.reduce((previous: number, current: T) => {
+
+        if (typeof extractFeatureFunction === 'function') {
+
+            const extractValue: number = extractFeatureFunction(current);
+
+            if (typeof extractValue === 'number') {
+                return previous + extractValue;
+            }
+            return NaN;
+        }
+
+        if (typeof current === 'number') {
+
+            return previous + current;
+        }
+        return NaN;
+    }, 0);
+
+    if (isNaN(total)) {
+        return NaN;
+    }
+
+    return total / list.length;
 };
